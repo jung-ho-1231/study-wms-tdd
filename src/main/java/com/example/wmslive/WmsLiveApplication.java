b9a91348ce6031be2511b551be54a429eb46b097
jung-ho-1231 <parkjung-1231@naver.com>
2023-08-01 01:35:33 +0900

    Initial commit

---
+++ src/main/java/com/example/wmslive/WmsLiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.wmslive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsLiveApplication.class, args);
+    }
+
+}
